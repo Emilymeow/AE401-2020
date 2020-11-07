60f4cc082c45431a56a92646d61bc470155cbed0
Emilymeow <104213@stu.jnps.tp.edu.tw>
2020-11-07 12:02:41 +0800

    class6

---
+++ class6_1.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+while True:
+    hits = mc.events.pollBlockHits()
+    if len(hits)>0:
+        hit = hits[0]
+        x,y,z = hit.pos
+        block = mc.getBlock(x,y,z)
+        if block == 1:
+            mc.setBlock(x,y,z, 174)
+
+
